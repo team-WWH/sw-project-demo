f26d1d3482d34a72468674d47077e2f33c16f159
queyang <ucxz6646581@163.com>
2025-07-17 23:15:58 +0800

    7.17

--- src/main/java/com/example/wwh/Mapper/FeedbackMapper.java
+++ src/main/java/com/example/wwh/Mapper/FeedbackMapper.java
@@ -1,4 +1,19 @@
 package com.example.wwh.Mapper;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import java.util.List;
+import com.example.wwh.pojo.Listener;
+import com.example.wwh.pojo.Speaker;
+import com.example.wwh.pojo.Organizer;
+import com.example.wwh.pojo.Feedback;
+@Mapper
+public interface FeedbackMapper {
 
-public class FeedbackMapper {
+
+    //提交更新反馈
+    @Insert("INSERT INTO feedback (Fcontent, ListenerID, SpeechID) VALUES (#{Fcontent}, #{ListenerID}, #{SpeechID})")
+    void submitFeedback(Feedback feedback);
+
+
 }
--- src/main/java/com/example/wwh/service/FeedbackService.java
+++ src/main/java/com/example/wwh/service/FeedbackService.java
@@ -1,4 +1,25 @@
 package com.example.wwh.service;
 
+
+import com.example.wwh.Mapper.FeedbackMapper;
+import com.example.wwh.pojo.Feedback;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class FeedbackService {
+
+    @Autowired
+    private FeedbackMapper feedbackMapper;
+
+    // 提交反馈
+    public String submitFeedback(Feedback feedback) {
+        try {
+            feedbackMapper.submitFeedback(feedback);
+            return "成功提交";
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "提交失败";
+        }
+    }
 }
